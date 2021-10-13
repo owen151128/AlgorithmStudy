1ddc55814e4ec3c4ec09141bad3c4c7d87bd6a50
owen151128 <owen151128@gmail.com>
2021-10-13 15:16:34 +0900

    Modify min3 & Add min4 algorithm

--- src/main/c/211013/min4.c
+++ src/main/c/211013/min4.c
@@ -9,28 +9,31 @@ static inline void checkSmallerThenMin(int *min, int num) {
     *min = num <= *min ? num : *min;
 }
 
-int min3(int a, int b, int c) {
+int min4(int a, int b, int c, int d) {
     int min = INT_MAX;
 
     checkSmallerThenMin(&min, a);
     checkSmallerThenMin(&min, b);
     checkSmallerThenMin(&min, c);
+    checkSmallerThenMin(&min, d);
 
     return min;
 }
 
 int main(void) {
-    int a, b, c;
+    int a, b, c, d;
 
-    printf("세 정수의 최솟값을 구합니다.\n");
+    printf("네 정수의 최솟값을 구합니다.\n");
     printf("a의 값 : ");
     scanInt(&a);
     printf("b의 값 : ");
     scanInt(&b);
     printf("c의 값 : ");
     scanInt(&c);
+    printf("d의 값 : ");
+    scanInt(&d);
 
-    printf("최솟값은 %d 입니다.\n", min3(a, b, c));
+    printf("최솟값은 %d 입니다.\n", min4(a, b, c, d));
 
     return 0;
 }
